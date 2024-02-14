87e01e36197e428901c3171b38c1b2fc4e3f2740
Zayaan Rahman <rahmanzayaan43@gmail.com>
2024-02-14 17:44:53 -0500

    Made migrations

--- campusconnect/campusconnect/settings.py
+++ campusconnect/campusconnect/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    'authentication.ApiConfig',
+    'authentication.apps.AuthenticationConfig',
 ]
 
 MIDDLEWARE = [
