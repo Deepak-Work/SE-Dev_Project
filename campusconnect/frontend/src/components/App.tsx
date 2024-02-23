a886cf4f1aa64e80069ce9df61b3298acdcbb0dd
Zayaan <zayaanra@buffalo.edu>
2024-02-22 19:23:49 -0500

    Boilerplate complete for react - added routing

---
+++ campusconnect/frontend/src/components/App.tsx
@@ -0,0 +1,21 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+
+import Login from './Login';
+
+function App() {
+  return (
+    <div id="root">
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App;
---
+++ campusconnect/frontend/src/components/Login.tsx
@@ -0,0 +1,9 @@
+import '../index.css';
+
+const Login = () => {
+    return (
+        <p>Hello World</p>
+    )
+}
+
+export default Login;
