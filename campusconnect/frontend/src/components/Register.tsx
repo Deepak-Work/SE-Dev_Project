b1d701a430ca483f191634fe43dacdf7cbffcc85
Zayaan <zayaanra@buffalo.edu>
2024-02-29 12:48:43 -0500

    Switched to MUI components; register functionality #6 and register page remodeled #4

--- campusconnect/frontend/src/components/Register.tsx
+++ campusconnect/frontend/src/components/Register.tsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
-import FormTextElement from "./UI/FormElement";
 import { useNavigate } from "react-router-dom";
-// import Cookies from "js-cookie";
-
 
 import Cookies from 'js-cookie';
+
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 interface Form {
     email: string;
@@ -14,71 +21,114 @@ interface Form {
     last_name: string;
 }
 
-function validateForm(form: Form): boolean {    
-    /* TODO:
-    1) Validate email - is it a valid NYU email?
-    2) Validate password - Use regex
-    */
-    
-    if (form.username.length < 10) {
-        return false;
-    }
-    return true;
+interface Errors {
+    email: boolean;
+    password: boolean;
+    username: boolean;
 }
 
 const Register = () => {
+    const defaultTheme = createTheme();
 
     const navigate = useNavigate();
 
-    const [form, setForm] = useState<Form>({
-        email: '',
-        password: '',
-        username: '',
-        first_name: '',
-        last_name: ''
+    const [errors, setErrors] = useState<Errors>({
+        email: false,
+        password: false,
+        username: false,
     });
 
-    const handleSubmit = async(e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
-        // Prevent default behavior for forms. We need this other some browsers (like Firefox) blocks the request.
-        e.preventDefault();
-
-        if (validateForm(form)) {
-            const headers = {
-                'Content-Type': 'application/json',
-                'X-CSRFToken': Cookies.get('csrftoken') || '',
-            }
-
-            const response: Response = await fetch('api/authentication/register', {
-                method: 'POST',
-                headers: headers,
-                body: JSON.stringify(form)
-            })
-
-            if (response.ok) {
-                navigate("/login");
-                console.log("Registration successful");
-            } else {
-                console.log("Registration failed");
-            }
+    const handleUsernameChange = (event: any) => {
+        if (event.target.validity.valid) {
+            setErrors({...errors, username: false});
+        } else {
+            setErrors({...errors, username: true});
         }
     }
 
-    return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-2">
-    <div className="p-6 space-y-8 bg-white rounded shadow-md w-96">
-        <h1 className="text-2xl font-bold text-center">Register Page</h1>
-        <form method="POST" className="space-y-5">
-            <FormTextElement onChange={(e) => setForm({...form, email: e.currentTarget.value})} type="email" id="exampleFormControlInput1" placeholder="Enter Email" label="Email" className="w-full p-2 border border-gray-300 rounded"/>
-            <FormTextElement onChange={(e) => setForm({...form, password: e.currentTarget.value})} type="password" id="exampleFormControlInput2" placeholder="Enter Password" label="Password" className="w-full p-2 border border-gray-300 rounded"/>
-            <FormTextElement onChange={(e) => setForm({...form, username: e.currentTarget.value})} type="username" id="exampleFormControlInput3" placeholder="Enter Username" label="Username" className="w-full p-2 border border-gray-300 rounded"/>
-            <FormTextElement onChange={(e) => setForm({...form, first_name: e.currentTarget.value})} type="firstname" id="exampleFormControlInput4" placeholder="Enter Firstname" label="Firstname" className="w-full p-2 border border-gray-300 rounded"/>
-            <FormTextElement onChange={(e) => setForm({...form, last_name: e.currentTarget.value})} type="lastname" id="exampleFormControlInput5" placeholder="Enter Lastname" label="Lastname" className="w-full p-2 border border-gray-300 rounded"/>
-            <input type="submit" value="Submit" onClick={handleSubmit} className="w-full p-2 text-white bg-blue-600 rounded hover:bg-blue-700">
-            </input>
-        </form>
-    </div>
-</div>
+    const handleEmailChange = (event: any) => {
+        if (event.target.validity.valid) {
+            setErrors({...errors, email: false});
+        } else {
+            setErrors({...errors, email: true});
+        }  
+    }
 
+    const handlePasswordChange = (event: any) => {
+        if (event.target.validity.valid) {
+            setErrors({...errors, password: false});
+        } else {
+            setErrors({...errors, password: true});
+        }
+    }
+
+    const handleSubmit = async(event: React.FormEvent<HTMLFormElement>) => {
+        // Prevent default behavior for forms. We need this other some browsers (like Firefox) blocks the request.
+        event.preventDefault();
+
+        const data = new FormData(event.currentTarget);
+        const form: Form = {
+            email: data.get('email') as string,
+            password: data.get('password') as string,
+            username: data.get('username') as string,
+            first_name: data.get('first_name') as string,
+            last_name: data.get('last_name') as string,
+        }
+
+        const headers = {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': Cookies.get('csrftoken') || '',
+        }
+
+        const response: Response = await fetch('api/authentication/register', {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(form)
+        })
+
+        if (response.ok) {
+            navigate("/login");
+            console.log("Registration successful");
+        } else {
+            console.log("Registration failed");
+        }
+
+    }
+
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline/>
+                <Box sx={{marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                    <Typography component="h1" variant="h5">Register</Typography>
+                    <Box component="form" onSubmit={handleSubmit} sx={{mt: 3}}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={6}>
+                                <TextField autoComplete="given-name" name="first_name" required fullWidth id="firstName" label="First Name" autoFocus/>
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField autoComplete="family-name" required fullWidth name="last_name" id="lastName" label="Last Name"/>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField onChange={handleUsernameChange} error={errors.username} helperText={errors.username ? 'Username must be at least 10 characters long' : ''} 
+                                inputProps={{pattern: '.{10,}'}} autoComplete="username" required fullWidth id="username" name="username"label="Username"/>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField onChange={handleEmailChange} error={errors.email} helperText={errors.email ? 'Enter a valid NYU email address (ends in @nyu.edu)' : ''} 
+                                inputProps={{pattern: '.*\@nyu\.edu$'}} autoComplete="email" required fullWidth id="email" name="email"label="Email Address"/>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField onChange={handlePasswordChange} error={errors.password} 
+                                helperText={errors.password ? 'Password needs at least 1 uppercase letter, 1 lowercase letter, 1 number, 1 special character, and at least 8 characters long': ''} 
+                                inputProps={{pattern: '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$'}}
+                                autoComplete="new-password" required fullWidth name="password" label="Password" type="password"id="password"/>
+                            </Grid>
+                        </Grid>
+                        <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>Register</Button>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
     );
 }
 
