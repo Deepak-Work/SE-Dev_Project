45a8ed3b8c91cac220f3c7eb12788a5c96920804
Zayaan <zayaanra@buffalo.edu>
2024-03-24 15:13:00 -0400

    Fixed serializer issue; added club page url/view

--- campusconnect/clubs/serializers.py
+++ campusconnect/clubs/serializers.py
@@ -3,7 +3,7 @@ from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.contrib.auth.models import User
 from .models import Club
 
-class ClubSerializer(serializers.Serializer):
+class ClubSerializer(serializers.ModelSerializer):
     class Meta:
         model = Club
-        exclude = ('member_count',)
+        fields=('name', 'description', 'location', 'email', 'contact', 'website')
--- campusconnect/clubs/views.py
+++ campusconnect/clubs/views.py
@@ -4,6 +4,8 @@ from rest_framework.views import APIView
 from .models import Club
 from rest_framework import status
 
+from .models import Club
+
 from .serializers import ClubSerializer
 
 # Create your views here.
@@ -12,28 +14,40 @@ class CreateClubView(APIView):
     
     def post(self, request):
         serializer = self.serilizer_class(data=request.data)
+
+        print(request.user.is_authenticated)
         
         if serializer.is_valid():
+
             clubName = serializer.data.get('name')
             clubDesc = serializer.data.get('description')
             clubLoc = serializer.data.get('location')
             clubEmail = serializer.data.get('email')
             clubContact = serializer.data.get('contact')
             clubWebsite = serializer.data.get('website')
-            clubOrganizer = serializer.data.get('organizer')
-            
-            queryset = Club.objects.filter(club_name=clubName)
+            clubOrganizer = request.session['id']
+                        
+            queryset = Club.objects.filter(name=clubName)
             if queryset.exists():
                 return Response(status=status.HTTP_400_BAD_REQUEST)
-            
+                        
             # TODO - Should we immediately put the club organizer into Follows model?
             
-            club = Club.objects.create_club(club_name=clubName, club_description=clubDesc, club_location=clubLoc, 
-                                            club_email=clubEmail, club_contact=clubContact, club_website=clubWebsite, club_organizer=clubOrganizer)
+            club = Club.objects.create(name=clubName, description=clubDesc, location=clubLoc, 
+                                            email=clubEmail, contact=clubContact, website=clubWebsite, organizer=clubOrganizer)
             club.save()
+
+            
             
             return Response(status=status.HTTP_201_CREATED)
         
         return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+class GetClubView(APIView):
+    def get(self, request, name, id):
+        # TODO: Eventually, we'll also have to include posts, events, and members associated with this club
+        c = Club.objects.filter(name=name, id=id).values().first()
+        return Response(c)
+
             
             
