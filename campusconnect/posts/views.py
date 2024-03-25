b950f34a66a263f07454956f2628f67c2ce6475e
Anish <anishnimbalkar@gmail.com>
2024-03-25 14:54:17 -0400

    posts app

---
+++ campusconnect/posts/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+
+class PostView(APIView):
+
+    pass
+
+class GetPostView(APIView):
+
+    pass
+
+class EditPostView(APIView):
+
+    pass
+
+class DeletePostView(APIView):
+    
+    pass
+
+
