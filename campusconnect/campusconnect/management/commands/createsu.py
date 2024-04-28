960384a708a89c1a78300accf588056059eba147
Zayaan Rahman <rahmanzayaan43@gmail.com>
2024-04-28 19:14:15 -0400

    updates for eb

---
+++ campusconnect/campusconnect/management/commands/createsu.py
@@ -0,0 +1,16 @@
+# images/management/commands/createsu.py
+
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Creates a superuser.'
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username='admin',
+                password='complexpassword123'
+            )
+        print('Superuser has been created.')
--- campusconnect/campusconnect/settings.py
+++ campusconnect/campusconnect/settings.py
@@ -95,7 +95,19 @@ WSGI_APPLICATION = "campusconnect.wsgi.application"
 # }
 
 # postgresql DB settings
-DATABASES = {
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'campusconnect',
